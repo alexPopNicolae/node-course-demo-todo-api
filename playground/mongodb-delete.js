68028417863bcc7738e88e8921abc4efddffb29e
Alex Pop <n.pop@accenture.com>
2018-09-18 15:22:20 +0300

    added some more new methods to the delete function

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -22,20 +22,44 @@ MongoClient.connect(
 
     //deleteOne
 
-    db.collection('Todos')
-      .deleteOne({ text: 'eat lunch' })
-      .then(
-        result => {
-          console.log('Statusul este: ');
-          console.log(result.result);
-        },
-        err => {
-          console.log('Could not perform operation because of error: ');
-          console.log(err);
-        }
-      );
+    // db.collection('Todos')
+    //   .deleteOne({ text: 'eat lunch' })
+    //   .then(
+    //     result => {
+    //       console.log('Statusul este: ');
+    //       console.log(result.result);
+    //     },
+    //     err => {
+    //       console.log('Could not perform operation because of error: ');
+    //       console.log(err);
+    //     }
+    //   );
 
     //findOneAndDelete
+
+    // db.collection('Todos')
+    //   .findOneAndDelete({ completed: false })
+    //   .then(result => {
+    //     console.log('Asta e ceea ce vine');
+    //     console.log(result);
+    //   });
+
+    //deleteMany users
+
+    db.collection('Users')
+      .deleteMany({ name: 'Alex Pop' })
+      .then(result => {
+        console.log('Statusul pentru deleteMany este: ');
+        console.log(result.result);
+      });
+
+    //findOneAndDelete Users
+    db.collection('Users')
+      .findOneAndDelete({ name: 'Alexandru' })
+      .then(result => {
+        console.log('Statusul pentru findOneAndDelete este: ');
+        console.log(result);
+      });
 
     // db.close();
   }
